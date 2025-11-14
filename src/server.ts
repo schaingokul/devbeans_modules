a69ccfd6e175ff711b5cd89d819aef7043f61dc7
Gokul <schaingokul@gmail.com>
2025-11-14 19:01:06 +0530

    middelware

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response, NextFunction} from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { pool } from './config/db';
 import cors from 'cors';
 import { initDatabase } from './core/dbInitializer';
@@ -9,8 +9,8 @@ import appConfig from './config/app.config';
 
 const app = express();
 
-app.use(cors({origin:"*"}));
-app.use(express.urlencoded({extended:true}));
+app.use(cors({ origin: "*" }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const server = http.createServer(app);
@@ -18,47 +18,47 @@ inistalizeSocket(server)
 // Core Logic
 
 export const printLog: boolean = process.env.printLog
-  ? process.env.printLog.toLowerCase() === 'true': true; // default to true if not set
+    ? process.env.printLog.toLowerCase() === 'true' : true; // default to true if not set
 console.log(printLog)
 
 // /* ------------------------------ 404 Handler ------------------------------ */
-// app.use((req, res, next) => {
-//   res.status(404).json({ success: false, error: 'Route Not Found' });
-// });
+app.use((req, res, next) => {
+    res.status(404).json({ success: false, error: 'Route Not Found' });
+});
 
 // /* ------------------------ Global Error Middleware ------------------------ */
 app.use(errorHandler)
 
 // Start server
 const startServer = async () => {
-  try {
-    // Test DB connection before starting
-    await pool.query('SELECT NOW()');
+    try {
+        // Test DB connection before starting
+        await pool.query('SELECT NOW()');
 
-    console.log('✅ Database connected successfully');
+        console.log('✅ Database connected successfully');
 
-    server.listen(appConfig.PORT, async() => {
-      await initDatabase();
-      // await testSession();
-      console.log(`🚀 Server running on port ${appConfig.PORT}`);
-    });
+        server.listen(appConfig.PORT, async () => {
+            await initDatabase();
+            // await testSession();
+            console.log(`🚀 Server running on port ${appConfig.PORT}`);
+        });
 
-    // Graceful shutdown
-    process.on('SIGINT', async () => {
-      console.log('\n⚡ Closing database pool...');
-      await pool.end();
-      process.exit(0);
-    });
+        // Graceful shutdown
+        process.on('SIGINT', async () => {
+            console.log('\n⚡ Closing database pool...');
+            await pool.end();
+            process.exit(0);
+        });
 
-    process.on('SIGTERM', async () => {
-      console.log('\n⚡ Closing database pool...');
-      await pool.end();
-      process.exit(0);
-    });
-  } catch (err) {
-    console.error('❌ Failed to connect to database', err);
-    process.exit(1); // stop server if DB fails
-  }
+        process.on('SIGTERM', async () => {
+            console.log('\n⚡ Closing database pool...');
+            await pool.end();
+            process.exit(0);
+        });
+    } catch (err) {
+        console.error('❌ Failed to connect to database', err);
+        process.exit(1); // stop server if DB fails
+    }
 };
 
 startServer();
